8bfc6ce82c7b79b59eda694a72c83c8c1b230071
Alaa <alaanourali@gmail.com>
2022-05-24 12:52:52 +0200

    chapter 1 1.5

--- chapter1/1.3.scm
+++ chapter1/1.3.scm
@@ -1,10 +1,11 @@
 #lang scheme/base
 
 
+(define (square x) (* x x))
 (define (largest-two-numbers x y z) ( cond ((or (> x y) (> x z)) (cond ((> y z) (values x y)) (else (values x z) ) ) ) (else (values y z)) ) )
 
 
-(define (sum-of-largest-two-numbers x y z) (let-values ( [(x1 y1) (largest-two-numbers x y z)] ) (+ x1 y1) ))
+(define (sum-of-largest-two-numbers x y z) (let-values ( [(x1 y1) (largest-two-numbers x y z)] ) (+ (square x1) (square y1) ) ))
 
 
-(sum-of-largest-two-numbers 10 15 3)
+(sum-of-largest-two-numbers 10 10 3)
